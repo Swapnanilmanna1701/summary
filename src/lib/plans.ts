9b18bb3e7b5d2e3e665074b1002611e14c01ebd3
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-07-29 20:51:45 +0530

    all utilities added

---
+++ src/lib/plans.ts
@@ -0,0 +1,32 @@
+export const plans = [
+    {
+      name: "Standard",
+      description: "For standard usage of the app",
+      price: {
+        monthly: 9,
+        yearly: 99,
+      },
+      features: [
+        "Basic Features Included",
+        "120 Words Limit",
+        "Access to Freeze Word",
+        "Sentence Fluency Analysis",
+        "Vocabulary Enhancement Suggestions",
+      ],
+    },
+    {
+      name: "Premium",
+      description: "For advanced usage and better analysis",
+      price: {
+        monthly: 19,
+        yearly: 149,
+      },
+      features: [
+        "All Basic Features",
+        "Extended Word Limit (up to 600 words)",
+        "Advanced Grammar Analysis",
+        "Plagiarism Checker",
+        "Priority Email Support",
+      ],
+    },
+  ];
